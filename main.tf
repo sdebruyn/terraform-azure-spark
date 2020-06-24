488b3dd82b15af7f9a5c7eb0f2986bcf335f5688
Sam Debruyn <sam@debruyn.dev>
2020-06-24 22:05:56 +0200

    give access to self

--- main.tf
+++ main.tf
@@ -25,6 +25,16 @@ resource "azurerm_key_vault" "kv" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
 }
 
+resource "azurerm_key_vault_access_policy" "self" {
+  key_vault_id            = azurerm_key_vault.kv.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = data.azurerm_client_config.current.object_id
+  secret_permissions      = local.kv_all_secret_permissions
+  storage_permissions     = local.kv_all_storage_permissions
+  key_permissions         = local.kv_all_key_permissions
+  certificate_permissions = local.kv_all_cert_permissions
+}
+
 # resource "azurerm_hdinsight_spark_cluster" "spark" {
 #   name                = "spark${local.solution}"
 #   location            = azurerm_resource_group.rg.location
