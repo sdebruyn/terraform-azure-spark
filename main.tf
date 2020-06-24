bda1d8e51093b00d2d1eb501b5e849fa9ed0da55
Sam Debruyn <sam@debruyn.dev>
2020-06-24 22:00:21 +0200

    add kv

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ locals {
   solution = "tfhdi"
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   location = "eastus2"
   name     = "rg${local.solution}"
@@ -18,3 +20,28 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
   account_tier             = "Standard"
 }
+
+resource "azurerm_key_vault" "kv" {
+  name                = "kv${local.solution}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+}
+
+# resource "azurerm_hdinsight_spark_cluster" "spark" {
+#   name                = "spark${local.solution}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   cluster_version     = "4.0"
+#   tier                = "Standard"
+
+#   component_version {
+#     spark = "2.4"
+#   }
+
+#   gateway {
+#     enabled = true
+
+#   }
+# }
