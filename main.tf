fe1c9436fd3a4d663ae387a49f65e86d216a1bf8
Sam Debruyn <sam@debruyn.dev>
2020-06-24 21:49:49 +0200

    create sa

--- main.tf
+++ main.tf
@@ -3,10 +3,18 @@ provider "azurerm" {
 }
 
 locals {
-  name = "solution"
+  solution = "tfhdi"
 }
 
 resource "azurerm_resource_group" "rg" {
   location = "eastus2"
-  name     = "rg${local.name}"
+  name     = "rg${local.solution}"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "sa${local.solution}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
 }
